834f1bd6df9de213bb0244ccb015885e07a61f5b
JAMES FRANK MENDOZA RIOS <21100654@ue.edu.pe>
2025-02-14 04:38:56 -0500

    5

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,12 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  Base:'https://james250920.github.io/sanv/',
   esbuild: {
     minify: true,
     target: 'esnext',
   },
-  Base:'https://james250920.github.io/sanv/',
+  build: {
+    outDir: 'docs',
+  },
 })
